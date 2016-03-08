2810bb67a2799bdccc5bd97a4e322791b0b6afc6
Andrew Shengilevych <wolgost13@gmail.com>
2016-03-08 13:06:52 +0200

    task part 2, add calculate proj area

--- js/controllers.js
+++ js/controllers.js
@@ -15,4 +15,24 @@ teamApp.controller('playersListCtrl',['$scope','$http', function($scope, $http) 
     	$scope.playersCount += count;
     	console.log($scope.playersCount);
     }
+
+    // Function for calculate proj
+
+    $scope.changeInput = function(player){
+    	// Set value to variable
+    	$scope.minutes = player.projectedStats.minutes.default;
+    	$scope.ur = player.projectedStats.usageRate.default;
+    	$scope.rr = player.projectedStats.reboundRate.default;
+    	$scope.ar = player.projectedStats.assistsRate.default;
+    	$scope.manEu = player.manualEffAdj.old;   
+    	//  Create array for get max value of ur, rr, ar
+    	// Clear array
+    	$scope.rateArr = [];    	
+    	//  Add to array actual value
+    	$scope.rateArr.push($scope.ur, $scope.rr, $scope.ar);
+    	//  Get max
+	    $scope.maxR = Math.max.apply(null, $scope.rateArr)
+	    //  Calculate proj and round it to 4 symbols after "."
+    	player.draftkings.projection.new = Math.round(($scope.minutes * $scope.manEu * ($scope.ur + $scope.rr + $scope.ar) / 3 / $scope.maxR) * 10000) / 10000;
+    }
 }]);
